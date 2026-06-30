e851915fa7fe468a86bacc35be92129d4e00cd77
Nicholas Norsek <nicknorsek01@gmail.com>
2026-06-29 21:42:21 -0400

    workout page logs

--- client/src/pages/Workouts.tsx
+++ client/src/pages/Workouts.tsx
@@ -1,12 +1,22 @@
 import Wrapper from "../components/Wrapper"
+import { useWorkoutHooks } from "../hooks/WorkoutHooks"
+import { Input }  from "../components/ui/input"
+import { Button } from "@/components/ui/button";
 
 const Workouts = () => {
+
+    const { loading, workouts, error, reload, reloader, handleCreateWorkout, handleEditWorkout, handleDeleteWorkout } = useWorkoutHooks();
+
+    console.log(workouts)
 
     return (
         <Wrapper>
             <div className="flex flex-col">
                 <h1 className="text-3xl font-bold py-5">Workouts</h1>
                 <p className="text-lg">Manage and build reusable programs</p>
+            </div>
+            <div className="flex mt-2 w-1/2">
+                <Input type="text" placeholder="Search workout..."/>
             </div>
         </Wrapper>
     )
